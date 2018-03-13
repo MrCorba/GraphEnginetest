79eca9b8b2ac60f176215386b543afe3c885af9a
Jesper Corba <jespercorba91@gmail.com>
2018-03-13 16:53:21 +0100

    Add minimal demo app

---
+++ GraphServer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trinity;
+
+namespace GraphServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrinityConfig.CurrentRunningMode = RunningMode.Embedded;
+            TrinityConfig.HttpPort = 3000;
+            
+            WordsStore server = new WordsStore();
+                        
+            server.Start();
+        }
+    }
+
+}
---
+++ GraphServer/WordsStore.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GraphEngineModels;
+using Trinity;
+
+
+namespace GraphServer
+{
+    public class WordsStore : GraphEngineModels.WordsStoreBase
+    {
+        public override void GetHandler(GetRequest request, out Word response)
+        {
+            try
+            {
+                response = Global.LocalStorage.LoadWord(request.word_id);
+            }
+            catch
+            {
+                response = new Word(description: "");
+            }
+        }
+
+        public override void CreateHandler(CreateRequest request, out Word response)
+        {
+            Word newWord = new Word(description: request.word);
+
+            Global.LocalStorage.SaveWord(newWord);
+
+            response = newWord;
+        }
+    }
+}
